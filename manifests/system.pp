7ac7e028bb50c2695944f355cb62c1041672591d
Prasad Chandrashekar <prasad.chandrashekar@reachlocal.com>
2014-11-04 15:41:10 +0530

    Adding proper rvm_installer path plus adding proper authentication

--- manifests/system.pp
+++ manifests/system.pp
@@ -8,8 +8,9 @@ class rvm::system($version=undef) {
 
   exec { 'system-rvm':
     path    => '/usr/bin:/usr/sbin:/bin',
-    command => "bash -c '/usr/bin/curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer && \
+    command => "bash -c '/usr/bin/curl -s https://raw.githubusercontent.com/wayneeseguin/rvm/master/binscripts/rvm-installer -o /tmp/rvm-installer && \
                 chmod +x /tmp/rvm-installer && \
+                gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3 && \
                 rvm_bin_path=/usr/local/rvm/bin rvm_man_path=/usr/local/rvm/man /tmp/rvm-installer --version ${actual_version} && \
                 rm /tmp/rvm-installer'",
     creates => '/usr/local/rvm/bin/rvm',
